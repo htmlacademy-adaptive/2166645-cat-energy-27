5cff55e37ba5d4f0ef3b05fe68d0ade272cb03cc
Inga <ivegon@mail.ru>
2023-01-26 17:07:05 +0700

    Изменяет изначальное закрытое состояние меню при работающем js

--- source/js/script.js
+++ source/js/script.js
@@ -1,14 +1,30 @@
 let navMain = document.querySelector(".nav");
 let navToggle = document.querySelector(".nav__toggle");
 
-navMain.classList.remove("nav--nojs");
+const closeMenu = () => {
+  navMain.classList.add("nav--closed");
+  navMain.classList.remove("nav--opened");
+};
+
+const openMenu = () => {
+  navMain.classList.remove("nav--closed");
+  navMain.classList.add("nav--opened");
+};
+
+const statusMenu = () => {
+  navMain.classList.remove("nav--nojs");
+  closeMenu();
+};
+
+if (navMain.classList.contains("nav--nojs")) {
+  statusMenu();
+}
+
 
 navToggle.addEventListener("click", function () {
   if (navMain.classList.contains("nav--closed")) {
-    navMain.classList.remove("nav--closed");
-    navMain.classList.add("nav--opened");
+    openMenu();
   } else {
-    navMain.classList.add("nav--closed");
-    navMain.classList.remove("nav--opened");
+    closeMenu();
   }
 });
